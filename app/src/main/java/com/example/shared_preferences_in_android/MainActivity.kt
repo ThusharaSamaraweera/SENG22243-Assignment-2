f4a5ede8042fb86ccfecbe93731db153ac27a6ec
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-03-27 10:44:46 +0530

    done ui

---
+++ app/src/main/java/com/example/shared_preferences_in_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shared_preferences_in_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
