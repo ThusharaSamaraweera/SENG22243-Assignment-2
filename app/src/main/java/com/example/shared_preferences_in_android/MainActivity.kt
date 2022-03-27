1387d05e8a5bf8f9c1bbc3d2583784a5668862a3
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-03-27 15:41:06 +0530

    add shared preferences

--- app/src/main/java/com/example/shared_preferences_in_android/MainActivity.kt
+++ app/src/main/java/com/example/shared_preferences_in_android/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.shared_preferences_in_android
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.shared_preferences_in_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sharedPref = getSharedPreferences("loginPref", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        binding.btnLogin.setOnClickListener{
+            val userName = binding.etUserName.text
+            val password = binding.etPassword.text
+            val isRememberMeOn = binding.switchRememberMe.isChecked
+
+            editor.apply {
+                putString("userName", userName.toString())
+                putBoolean("isRememberMeOn", isRememberMeOn)
+                apply()
+            }
+        }
+
+        binding.btnLoad.setOnClickListener {
+            val userName = sharedPref.getString("userName", "")
+            val isRememberMeOn = sharedPref.getBoolean("isRememberMeOn", false)
+
+            binding.tvUserNameOutput.text = userName
+            binding.tvRememberMeOutput.text = isRememberMeOn.toString()
+        }
+
+
     }
+
 }
